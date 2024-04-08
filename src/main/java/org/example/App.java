54c30b41b45259f83ce55a1f4be2ce635482d3ac
janvipravinhaipatel <158079903+janvipravinhaipatel@users.noreply.github.com>
2024-04-08 10:19:20 -0400

    firstcommit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class App extends{
+
+    public static String FizzBuzz(int a) {
+        String Output = " ";
+        return Output;
+        }
+        public static void main (String[] args)
+        {
+            System.out.println("Hello World!");
+        }
+    }
